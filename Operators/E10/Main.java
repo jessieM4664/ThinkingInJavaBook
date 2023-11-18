e521c0e3a7993dc74719f53068fc3e4cf978cfc5
Jessica Morrison <jessicamorrison8@rocketmail.com>
2023-11-18 16:06:50 -0500

    updated E10

--- Operators/E10/Main.java
+++ Operators/E10/Main.java
@@ -5,9 +5,14 @@ public class Main {
         int a = 9; // 1001
         int b = 12; // 1100
         int c = a & b;
+        int d = a | b;
+        int e = a ^ b;
+        int f = ~a;
 
-        System.out.println(Integer.toBinaryString(c));
+        System.out.println("AND: " + Integer.toBinaryString(c));
+        System.out.println("OR: " + Integer.toBinaryString(d));
+        System.out.println("XOR: " + Integer.toBinaryString(e));
+        System.out.println("NOT: " + Integer.toBinaryString(f));
 
-        // output of the compared bits in each index a and b: 1000
     }
 }
