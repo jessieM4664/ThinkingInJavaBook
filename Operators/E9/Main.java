51e7810cb7bfb90ce021995c028ce7a3453b0e6c
Jessica Morrison <jessicamorrison8@rocketmail.com>
2023-11-17 20:00:57 -0500

    E9 completed

---
+++ Operators/E9/Main.java
@@ -0,0 +1,11 @@
+package E9;
+
+public class Main {
+    public static void main(String args[]) {
+        float x = 1.39E3f; // output: 1390.0
+        double y = 70E2d;  // output: 7000.0
+
+        System.out.println("x: " + x);
+        System.out.println("y: " + y);
+    }
+}
