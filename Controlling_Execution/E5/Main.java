ae2a5a8de573203bbb939b2f616e405dfa2e06a7
Jessica Morrison <jessicamorrison8@rocketmail.com>
2023-11-19 16:57:22 -0500

    E4,5 done

--- Controlling_Execution/E5/Main.java
+++ Controlling_Execution/E5/Main.java
@@ -1,12 +1,11 @@
-package E4;
+package E5;
 
 public class Main {
     public static void main(String args[]) {
         for (int i = 1; i < 8; i++) {
             for (int j = 1; j < 8; j++) {
-                if (i%2!=0 && (i==j)) { // all prime numbers 
-                    System.out.println(i);
-                }
+                String output = (i%2!=0 && i==j) ? " " + i : ""; // all prime numbers 
+                System.out.print(output);
             }
         }
     }
