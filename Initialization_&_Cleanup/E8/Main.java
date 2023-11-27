08559a806bb8652089ddd1bbbc3cf5ec197b45ce
Jessica Morrison <jessicamorrison8@rocketmail.com>
2023-11-27 12:02:32 -0500

    E8 done

---
+++ Initialization_&_Cleanup/E8/Main.java
@@ -0,0 +1,17 @@
+package E8;
+
+public class Main {
+    public static void main(String args[]) {
+        new Main().method1();
+    }
+
+    Main method1() {
+        method2();
+        method2();
+        return this;
+    }
+
+    void method2() {
+        System.out.println("working");
+    }
+}
