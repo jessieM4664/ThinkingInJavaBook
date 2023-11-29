e3e5e01ef9ac8acd8b92c09591dc8a5146c7eee4
Jessica Morrison <jessicamorrison8@rocketmail.com>
2023-11-29 14:15:41 -0500

    E13 - static { }

---
+++ Initialization_&_Cleanup/E13/Main.java
@@ -0,0 +1,29 @@
+package E13;
+
+public class Main { // explicit static
+    public static void main(String args[]) {
+        System.out.println("Inside main()");
+        Cups.cup1.f(99); // 1
+    }
+}
+
+class Cup {
+    Cup(int marker) {
+        System.out.println("Cup(" + marker + ")");
+    }
+    void f(int marker) {
+        System.out.println("f(" +  marker + ")");
+    }
+}
+
+class Cups {
+    static Cup cup1;
+    static Cup cup2;
+    static {
+        cup1 = new Cup(1);
+        cup2 = new Cup(2);
+    }
+    Cups() {
+        System.out.println("Cups()");
+    }
+}
