b5c4ebbf47fb2349f148e87bcb0f78efe2a6fe70
Jessica Morrison <jessicamorrison8@rocketmail.com>
2023-11-29 18:11:46 -0500

    varargs in main method using command line arguments

--- Initialization_&_Cleanup/E20/Main.java
+++ Initialization_&_Cleanup/E20/Main.java
@@ -3,7 +3,7 @@ package E20;
 import java.util.Arrays;
 
 public class Main {
-    public static void main(String[] args) { // varargs in main method
+    public static void main(String... args) { // varargs in main method
         System.out.print(Arrays.toString(args));
 
         // command line argument:
