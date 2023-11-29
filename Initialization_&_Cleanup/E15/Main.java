f75348ff9acce51c398aecb7c958c350b428f3fe
Jessica Morrison <jessicamorrison8@rocketmail.com>
2023-11-29 16:09:52 -0500

    E16 - array intialization

---
+++ Initialization_&_Cleanup/E15/Main.java
@@ -0,0 +1,8 @@
+package E15;
+
+public class Main { // UNFINSIHSED
+    public static void main(String args[]) {
+        System.out.println("Inside main()");
+        
+    }
+}
---
+++ Initialization_&_Cleanup/E16/Main.java
@@ -0,0 +1,14 @@
+package E16;
+
+public class Main {
+    public static void main(String args[]) {
+        String str[] = new String[]
+        {"Hello", "I", "am", "Jessie", "Morrison",};
+
+        for (int i = 0; i < str.length; i++) {
+            System.out.print(str[i] + " ");
+            // prints each string next to eachother
+        }
+
+    }
+}
