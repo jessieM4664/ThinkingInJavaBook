26b8388ef9623646f0b529d4e9093ca222b6353f
Jessica Morrison <jessicamorrison8@rocketmail.com>
2023-12-01 20:53:55 -0500

    inheritance with constructors

---
+++ Reusing_Classes/E9/Root.java
@@ -0,0 +1,36 @@
+package E9;
+
+public class Root {
+    Root() {
+        System.out.println("Root Constructor");
+    }
+    public static void main(String args[]) {
+        new Component1();
+        new Component2();
+        new Component3();
+    }
+}
+
+class Stem extends Root {
+    Stem() {
+        System.out.println("Stem Constructor");
+    }
+}
+
+class Component1 {
+    Component1() {
+        System.out.println("Con1 Constructor");
+    }
+}
+
+class Component2 {
+    Component2() {
+        System.out.println("Con2 Constructor");
+    }
+}
+
+class Component3 {
+    Component3() {
+        System.out.println("Con3 Constructor");
+    }
+}
