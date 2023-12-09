204a0daa82d28e357eb2e193ab81a6cbe9251340
Jessica Morrison <jessicamorrison8@rocketmail.com>
2023-12-09 15:28:42 -0500

    singleton design, w/ lazy intialization

--- Thinking_In_Java_Patterns_Book2/The_Pattern_Concept/E1/A.java
+++ Thinking_In_Java_Patterns_Book2/The_Pattern_Concept/E1/A.java
@@ -1,6 +1,6 @@
 package The_Pattern_Concept.E1;
 
-public class A {
+final public class A { // final to undermine the clone() in Object class
     private static A instance = null;
     private A () { }
     public static A getInstance() {
