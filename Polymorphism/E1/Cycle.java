f6e4587f897acf519b0a9da2f9d99f7ee55ad759
Jessica Morrison <jessicamorrison8@rocketmail.com>
2023-12-09 21:42:44 -0500

    polymorphism

---
+++ Polymorphism/E1/Cycle.java
@@ -0,0 +1,29 @@
+package E1;
+
+
+public class Cycle {
+    public static void main(String args[]) {
+        Unicycle u = new Unicycle();
+        Bicycle b = new Bicycle();
+        Tricycle t = new Tricycle();
+
+        u.ride();
+        b.ride();
+        t.ride();
+    }
+    public void ride() {
+        System.out.println("ride()");
+    }
+}
+
+class Unicycle extends Cycle {
+
+}
+
+class Bicycle extends Cycle {
+
+}
+
+class Tricycle extends Cycle {
+
+}
--- Reusing_Classes/E22/Main.java
+++ Reusing_Classes/E22/Main.java
@@ -1,5 +1,6 @@
 package E22;
 
 final public class Main {
-    
+    // Mushroom extends Main
+    //! Error: Type Mushroom cannot subclass the final class Main
 }
