f25bf5ccf0df1cd7c75b8850e14e771a6023b7bd
Jessica Morrison <jessicamorrison8@rocketmail.com>
2023-12-10 02:29:56 -0500

    Designing with Inheritance

---
+++ Polymorphism/E15/Main.java
@@ -0,0 +1,29 @@
+package E15;
+
+
+public class Main {
+    public static void main(String args[]) {
+        Starship star = new Starship();
+        star.alert();
+        star.change();
+        star.alert();
+    }
+}
+
+class Starship {
+    private AlertStatus alertMessage = new Safe();
+    public void change() {alertMessage = new Critical();}
+    public void alert() {alertMessage.m();} 
+}
+
+class AlertStatus {
+    public void m() { }
+}
+
+class Safe extends AlertStatus {
+    public void m() {System.out.println("Safe()");}
+}
+
+class Critical extends AlertStatus {
+    public void m() {System.out.println("Critical()");}
+}
