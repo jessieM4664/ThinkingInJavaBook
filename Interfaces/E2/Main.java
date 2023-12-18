a6a53ea6decaddde2cb5ab9ca10b8d991a741cc5
Jessica Morrison <jessicamorrison8@rocketmail.com>
2023-12-17 20:27:44 -0500

    cannot inherit abstract classes without methods

---
+++ Interfaces/E2/Main.java
@@ -0,0 +1,12 @@
+package E2;
+
+public class Main {
+    public static void main(String args[]) {
+        A a = new A();
+        //! ERROR: Cannot instantiate the type A class
+    }
+}
+
+abstract class A {
+
+}
