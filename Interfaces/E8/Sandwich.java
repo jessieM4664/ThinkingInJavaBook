98334850dff940b48dc20fb146dfcdadae51413d
Jessica Morrison <jessicamorrison8@rocketmail.com>
2023-12-19 17:12:56 -0500

    interface visibility/reflection implementation

---
+++ Interfaces/E8/Sandwich.java
@@ -0,0 +1,29 @@
+package E8;
+
+interface FastFood {
+    void print();
+}
+
+public class Sandwich extends PortableLunch {
+    Bread b = new Bread();
+    Pickle p = new Pickle();
+    Sandwich() {System.out.println("Sandwich()");}
+    public static void main(String arg[]) {
+        new Sandwich();
+    }
+}
+class PortableLunch extends Lunch {
+    PortableLunch() {System.out.println("PortableLunch()");}
+}
+class Lunch extends Meal {
+    Lunch() {System.out.println("Lunch()");}
+}
+class Bread {
+    Bread() {System.out.println("Bread()");}
+}
+class Meal {
+    Meal() {System.out.println("Meal()");}
+}
+class Pickle {
+    Pickle() {System.out.println("Pickle()");}
+}
