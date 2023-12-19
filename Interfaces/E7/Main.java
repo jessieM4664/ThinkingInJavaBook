387e2d61809673e10e2da00f9ce34135ca8fae39
Jessica Morrison <jessicamorrison8@rocketmail.com>
2023-12-18 19:25:55 -0500

    interface visibility/reflection implementation

---
+++ Interfaces/E7/Main.java
@@ -0,0 +1,31 @@
+package E7;
+
+interface Rodent {
+    void move();
+}
+
+public class Main {
+    public static void main(String args[]) {
+        Rodent rodents[] = {
+            new Mouse(), new Gerbil(), new Hampster() };
+
+        for (int i = 0; i < rodents.length; i++) {
+            rodents[i].move();
+        }
+    }
+}
+class Mouse implements Rodent {
+    public void move() {
+        System.out.println("Mouse scurrying");
+    }
+}
+class Gerbil implements Rodent {
+    public void move() {
+        System.out.println("Gerbil scurrying");
+    }
+}
+class Hampster implements Rodent {
+    public void move() {
+        System.out.println("Hampster scurrying");
+    }
+}
